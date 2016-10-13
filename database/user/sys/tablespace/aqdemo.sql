2ad3bfeefe994fe7281212da895095225a099794
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2016-10-13 15:05:25 +0200

    change default tablespace location

--- database/user/sys/tablespace/aqdemo.sql
+++ database/user/sys/tablespace/aqdemo.sql
@@ -1,3 +1,3 @@
 CREATE TABLESPACE aqdemo
-   DATAFILE '/u01/app/oracle/oradata/phscdb/phspdb2/aqdemo1.dbf' SIZE 10M
+   DATAFILE '/u01/app/oracle/oradata/odb/aqdemo1.dbf' SIZE 10M
    AUTOEXTEND ON NEXT 5M MAXSIZE 100M;
